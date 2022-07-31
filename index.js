cf589901db492cd2eb63f20d6e2bad29db7ba498
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-07-30 22:27:12 -0300

    add initial configuration

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+const mySecret = process.env.MONGO_URI;
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+mongoose.connect(mySecret, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(cors());
 
@@ -19,6 +25,9 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post('/api/shorturl/', (req, res, next) => {
+res.json({response : 'Hi'})
+})
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
